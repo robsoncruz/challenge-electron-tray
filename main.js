317d8051fa1a330aaf6d51c5df786ee83b72001e
Robson da Cruz Vieira <robson@HINOVA.net>
2020-04-21 15:33:36 -0300

    removendo app.dock.hide

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { resolve } = require('path');
 const { app, Menu, Tray } = require('electron');
 
-app.dock.hide();
-
 app.on('ready', () => {
     const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));    
     
